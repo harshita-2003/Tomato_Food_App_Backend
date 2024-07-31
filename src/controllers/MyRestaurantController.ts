a845548d874fa4202ea5775a75741b36b64fb7c2
harshita-2003 <harshitag374@gmail.com>
2024-08-01 02:00:27 +0530

    adding restaurant apis

---
+++ src/controllers/MyRestaurantController.ts
@@ -0,0 +1,98 @@
+import { Request, Response } from "express";
+import Restaurant from "../models/restaurant";
+import cloudinary from "cloudinary";
+import mongoose from "mongoose";
+
+const getMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    const getRestaurant = await Restaurant.findOne({ user: req.userId });
+    if (!getRestaurant) {
+      return res
+        .status(404)
+        .json({ message: "Restaurant for this User doesn't exist" });
+    }
+
+    res.status(200).json(getRestaurant);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Error fetching restaurant" });
+  }
+};
+
+const createMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    console.log("Request received:", req.body);
+    console.log("User ID:", req.userId);
+
+    const existingRestaurant = await Restaurant.findOne({ user: req.userId });
+    if (existingRestaurant) {
+      res.status(409).json({ message: "Restaurant already exists!!" });
+      //error 409 : duplicate
+    }
+
+    //Image converted to base64 format
+    // const image = req.file as Express.Multer.File;
+    // const base64Image = Buffer.from(image.buffer).toString("base64");
+    // const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+    // const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
+
+    const imageUrl = await uploadImage(req.file as Express.Multer.File)
+
+    const restaurant = new Restaurant(req.body);
+    restaurant.imageUrl = imageUrl;
+    restaurant.user = new mongoose.Types.ObjectId(req.userId);
+    restaurant.lastUpdated = new Date();
+
+    await restaurant.save();
+    res.status(201).send(restaurant);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong!!", error });
+  }
+};
+
+export const updateMyRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.userId });
+    if (!restaurant) {
+      return res.status(404).json({ message: "restaurant doesn't exist" });
+    }
+
+    restaurant.restaurantName = req.body.restaurantName;
+    restaurant.city = req.body.city;
+    restaurant.country = req.body.country;
+    restaurant.deliveryPrice = req.body.deliveryPrice;
+    restaurant.estimatedDeliveryTime = req.body.estimatedDeliveryTime;
+    restaurant.cuisines = req.body.cuisines;
+    restaurant.menuItems = req.body.menuItems;
+    restaurant.lastUpdated = new Date();
+
+    if(req.file) {
+      const imageUrl = await uploadImage(req.file as Express.Multer.File)
+      restaurant.imageUrl = imageUrl;
+    }
+
+    await restaurant.save();
+    res.status(200).send(restaurant);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+const uploadImage = async (file : Express.Multer.File) => {
+
+  //Image converted to base64 format
+  const image = file;
+  const base64Image = Buffer.from(image.buffer).toString("base64");
+  const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+  const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
+  return uploadResponse.url;
+
+};
+
+export default {
+  getMyRestaurant,
+  createMyRestaurant,
+  updateMyRestaurant,
+};
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,30 @@ import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose"
 import myUserRoute from "./routes/MyUserRoute"
+import { v2 as cloudinary } from "cloudinary"
+import myRestaurantRoute from "./routes/MyRestaurantRoute"
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(()=> console.log("🍀 Mongodb database connected"))
+
+//Cloudinary for image 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
     
 const app = express()
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173', // Adjust this as needed
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
   
 // Middleware to log all incoming requests
 app.use((req, res, next) => {
@@ -23,11 +39,12 @@ app.get("/check" , async(req: Request, res: Response) => {
 })
 
 app.use("/api/my/user" , myUserRoute)
+app.use("/api/my/restaurant" , myRestaurantRoute)
 
 app.get("/test" , async (req,res) => {
-    res.json({ message : "Hello world" });
+  res.json({ message : "Hello world" });
 })
 
 app.listen(7000, () => {
-    console.log("⚙️  Server running on port 7000")
+  console.log("⚙️  Server running on port 7000")
 })
